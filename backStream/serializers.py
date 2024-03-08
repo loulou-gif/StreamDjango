2af039277b4fe0e48796c14d4edcd14bc08df651
unknown <konankanjulius10@gmail.com>
2024-03-08 14:35:48 +0000

    user config création

--- backStream/serializers.py
+++ backStream/serializers.py
@@ -3,9 +3,17 @@ from django.contrib.auth.models import User
 from .models import Streams, News
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True) 
+    # userdetail = UserDetailSerializer(required=False)
+
     class Meta:
-        model= User
-        fields= "__all__"
+        model = User
+        fields = ('id', 'username', 'email', 'first_name', 'last_name','is_staff', 'is_active', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
         
         
 class StreamSerializer(serializers.ModelSerializer):
